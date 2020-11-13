037b437b764804085a98a8fcb8a72de949321d45
Magnus <beachmagnus+git@gmail.com>
2020-11-13 23:35:51 +0100

    removing unessescary files, start hard coding list

---
+++ frontend/react/v1/src/compnents/users-list/usersList.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserList = () => (
+    <div className="user-list">
+        <h1>Navn</h1>
+        <ul>
+            <li>Magnus</li>
+            <li>Hanne</li>
+            <li>Bjørn</li>
+            <li>Sunniva</li>
+        </ul>
+    </div>
+);
+export default UserList;
