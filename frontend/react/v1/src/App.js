bc2f211757940b7172cc48793e30cbb372ceafef
Magnus <beachmagnus+git@gmail.com>
2020-11-14 15:20:51 +0100

    styling, wishlost component

--- frontend/react/v1/src/App.js
+++ frontend/react/v1/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import UserList from "./compnents/users-list/usersList.component.jsx";
+import "./compnents/main-container/MainContainer.component.jsx";
+import MainContainer from './compnents/main-container/MainContainer.component.jsx';
 
 function App() {
   return (
     <div className="App">
       <UserList />
+      <MainContainer />
     </div>
   );
 }
---
+++ frontend/react/v1/src/compnents/wishList/WishList.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./WishList.component.css";
+
+const WishList = ()=> (
+    <div className="WishList">
+        <h1>Wishlist component</h1>
+    </div>
+);
+
+export default WishList;
