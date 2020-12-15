b34613dee47b4e2db796cb4a5975d268303f97c4
Magnus <beachmagnus+git@gmail.com>
2020-12-16 00:03:59 +0100

    we are back in buisness!

--- frontend/react/v2/src/App.js
+++ frontend/react/v2/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+import UserList from './components/user-list/user-list.component'
+import HomePage from './pages/homepage/homepage.component'
+import {Switch, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserList />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
---
+++ frontend/react/v2/src/components/user-list/user-list.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserList = () => (
+  <div className="user-list">
+    <h1>User list</h1>
+  </div>
+);
+
+export default UserList;
