b9bdc529a08651a2b79cdfea939a70083264739a
Magnus <beachmagnus+git@gmail.com>
2020-12-16 00:27:51 +0100

    lagde sign in-componenten, sammen med beholder og link fra hovedsida

--- frontend/react/v2/src/App.js
+++ frontend/react/v2/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import UserList from './components/user-list/user-list.component'
 import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom'
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.page';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <UserList />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/sign-in-sign-up" component={SignInSignUp} />
       </Switch>
     </div>
   );
---
+++ frontend/react/v2/src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = () => (
+  <div className="sign-in">
+    <h1>Sign up component</h1>
+  </div>
+);
+
+export default SignUp;
