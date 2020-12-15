25198c94dfa4f55e7c31652a34a55d98a8d08645
Magnus <beachmagnus+git@gmail.com>
2020-12-16 00:12:45 +0100

    basic styling

--- frontend/react/v2/src/App.js
+++ frontend/react/v2/src/App.js
@@ -7,7 +7,7 @@ import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <UserList />
       <Switch>
         <Route exact path="/" component={HomePage} />
--- frontend/react/v2/src/components/user-list/user-list.component.jsx
+++ frontend/react/v2/src/components/user-list/user-list.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./user-list.styles.css";
+
 const UserList = () => (
   <div className="user-list">
     <h1>User list</h1>
