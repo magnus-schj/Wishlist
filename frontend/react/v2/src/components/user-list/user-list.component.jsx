6744e1202c75f64b0f5102a3b04765c86b490ed3
Magnus <beachmagnus+git@gmail.com>
2020-12-16 12:39:09 +0100

    slipper å hard-kode navn på lista

--- frontend/react/v2/src/components/user-list/user-list.component.jsx
+++ frontend/react/v2/src/components/user-list/user-list.component.jsx
@@ -2,10 +2,32 @@ import React from "react";
 
 import "./user-list.styles.css";
 
-const UserList = () => (
-  <div className="user-list">
-    <h1>User list</h1>
-  </div>
-);
+class UserList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        { id: 1, name: "Magnus" },
+        { id: 2, name: "Hanne" },
+        { id: 3, name: "Bjorn" },
+        { id: 4, name: "Sunniva" },
+      ],
+    };
+  }
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div className="user-list">
+        <h1>User list</h1>
+        <ul>
+          {users.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default UserList;
