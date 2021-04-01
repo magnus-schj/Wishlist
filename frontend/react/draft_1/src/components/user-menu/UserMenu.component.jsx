6d0fe13e29be23c636945d9e3b4d202cd3f30dc5
Magnus <beachmagnus+git@gmail.com>
2021-04-01 21:00:35 +0200

    map mock data to names

--- frontend/react/draft_1/src/components/user-menu/UserMenu.component.jsx
+++ frontend/react/draft_1/src/components/user-menu/UserMenu.component.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import MOCK_DATA from '../../assets/MOCK_DATA'
+import React from "react";
+import MOCK_DATA from "../../assets/MOCK_DATA";
 
 const UserMenu = () => {
-    console.log(MOCK_DATA)
-    return(
-<h1>Test</h1>
-    )
-}
+  console.log(MOCK_DATA);
+  return (
+    <div className="user-menu">
+      <h1>Test</h1>
+      {MOCK_DATA.map((user) => {
+        return <li>{user.name}</li>;
+      })}
+    </div>
+  );
+};
 
-
-export default UserMenu
+export default UserMenu;
