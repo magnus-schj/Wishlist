ccc4cdbaf0517926f768021db19dd9bba8cb4afc
Magnus <beachmagnus+git@gmail.com>
2021-04-01 20:52:16 +0200

    first steps

---
+++ frontend/react/draft_1/src/assets/MOCK_DATA.js
@@ -0,0 +1,7 @@
+const MOCK_DATA = [
+    {name: "Magnus", wishes:["PLaystaions 5","TV","iPhone"]},
+    {name:"Hane",wishes:["Duk","tallerkener","Blomster"]},
+    {name:"Sunniva",wishes:["Tegneting","Hun i kosebamse"]}
+]
+
+export default MOCK_DATA
---
+++ frontend/react/draft_1/src/components/user-menu/UserMenu.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MOCK_DATA from '../../assets/MOCK_DATA'
+
+const UserMenu = () => {
+    console.log(MOCK_DATA)
+    return(
+<h1>Test</h1>
+    )
+}
+
+
+export default UserMenu
