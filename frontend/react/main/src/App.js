01ea16fe995f84a0f4a54c26f63c719fdbf375d6
Magnus <beachmagnus+git@gmail.com>
2021-04-03 15:17:02 +0200

    every user has its own page

--- frontend/react/main/src/App.js
+++ frontend/react/main/src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import UserMenu from "./components/user-menu/UserMenu.component";
 import MOCK_DATA from "./assets/MockData";
 import { Route } from "react-router-dom";
+import WishList from "./components/wish-list/WishList.component";
+
 const App = () => {
   return (
     <div className="App">
       <UserMenu />
       <h1>Ønskelister</h1>
-      {/* {MOCK_DATA.map((user) => {
-        <Route exact path={`/${user.name}`} component={}/>;
-      })} */}
+      {MOCK_DATA.map((user) => {
+        const component = <WishList name={user.name} wishes={user.wishes} />;
+        return <Route exact path={`/${user.name}`} render={() => component} />;
+      })}
     </div>
   );
 };
--- frontend/react/main/src/assets/MockData.js
+++ frontend/react/main/src/assets/MockData.js
@@ -1,9 +1,9 @@
 const MOCK_DATA = [
-  { name: "User 1", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User 2", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User 3", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User 4", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User 5", wishes: ["wish 1", "wish 2", "wish 3"] },
+  { name: "User1", wishes: ["wish 1", "wish 2", "wish 3"] },
+  { name: "User2", wishes: ["wish 1", "wish 2", "wish 3"] },
+  { name: "User3", wishes: ["wish 1", "wish 2", "wish 3"] },
+  { name: "User4", wishes: ["wish 1", "wish 2", "wish 3"] },
+  { name: "User5", wishes: ["wish 1", "wish 2", "wish 3"] },
 ];
 
 export default MOCK_DATA;
