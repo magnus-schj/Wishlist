58ca4c9ad85c7d9e60790f96ad55b664df78d29d
Magnus <beachmagnus+git@gmail.com>
2021-04-03 18:25:29 +0200

    main page

--- frontend/react/main/src/App.js
+++ frontend/react/main/src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import UserMenu from "./components/user-menu/UserMenu.component";
 import MOCK_DATA from "./assets/MockData";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import WishPage from "./pages/wish-page/WishPage.component";
+import Main from "./pages/main/Main.component";
 
 const App = () => {
   return (
     <div className="App">
       <UserMenu />
       <div className="header">
-        <h1>Ønskelister</h1>
+        <Link to="/">
+          <h1>Ønskelister</h1>
+        </Link>
       </div>
+      <Route exact path="/" component={Main} />
       {MOCK_DATA.map((user) => {
         const component = <WishPage name={user.name} wishes={user.wishes} />;
         return <Route exact path={`/${user.name}`} render={() => component} />;
---
+++ frontend/react/main/src/pages/main/Main.component.jsx
@@ -0,0 +1,15 @@
+import "./main.styles.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h2>Velkommen til den nye ønskeliste-siden!</h2>
+      <h2>
+        Den er per nå work in progress, så hvis du har noen tilbakemeldinger,
+        eller merker at noe er galt, send meg melding!
+      </h2>
+    </div>
+  );
+};
+
+export default Main;
