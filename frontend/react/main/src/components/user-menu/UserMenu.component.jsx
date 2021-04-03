22f9c9de49c1d7d6847fcdd4d60d1cba6550811a
Magnus <beachmagnus+git@gmail.com>
2021-04-03 14:44:24 +0200

    styling

--- frontend/react/main/src/components/user-menu/UserMenu.component.jsx
+++ frontend/react/main/src/components/user-menu/UserMenu.component.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import MOCK_DATA from "../../assets/MockData";
+import "./UserMenu.styles.css";
 
 const UserMenu = () => (
   <div className="user-menu">
-    <h1>User</h1>
+    <h1>Users</h1>
+    <div className="user-list">
+      <ul>
+        {MOCK_DATA.map((user) => {
+          return <li>{user.name}</li>;
+        })}
+      </ul>
+    </div>
   </div>
 );
 
