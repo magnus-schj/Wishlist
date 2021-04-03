8efb1d0577e220597472cdf83bd478e0faf15aa9
Magnus <beachmagnus+git@gmail.com>
2021-04-03 18:51:28 +0200

    <input type="password" /> main is different ending on isLoggedIn

--- frontend/react/main/src/pages/main/Main.component.jsx
+++ frontend/react/main/src/pages/main/Main.component.jsx
@@ -8,6 +8,25 @@ const Main = ({ isLoggedIn }) => {
         Den er per nå work in progress, så hvis du har noen tilbakemeldinger,
         eller merker at noe er galt, send meg melding!
       </h2>
+      <br />
+      {isLoggedIn ? (
+        <h2>Du er logget inn.</h2>
+      ) : (
+        <div className="log-in">
+          <h2>Du er for øyeblikket ikke logget inn.</h2>
+          <h2>Logg inn her:</h2>
+          <span>
+            Brukernavn:
+            <input type="text" /> <br />
+          </span>
+          <span>
+            Passord:
+            <input type="password" />
+          </span>
+          <br />
+          <button>Logg inn</button>
+        </div>
+      )}
     </div>
   );
 };
