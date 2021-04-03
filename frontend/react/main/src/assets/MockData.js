066ec71deee52d944a173ade04044d37431ce720
Magnus <beachmagnus+git@gmail.com>
2021-04-03 15:42:49 +0200

    mer mock data

--- frontend/react/main/src/assets/MockData.js
+++ frontend/react/main/src/assets/MockData.js
@@ -1,9 +1,27 @@
 const MOCK_DATA = [
-  { name: "User1", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User2", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User3", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User4", wishes: ["wish 1", "wish 2", "wish 3"] },
-  { name: "User5", wishes: ["wish 1", "wish 2", "wish 3"] },
+  {
+    name: "Bjorn",
+    wishes: [
+      "Bøker (SciFi, Fantasy, thriller, ...)",
+      "CD'er med ny, spennende musikk",
+      "Bok: Robin Hobb: Fool's Errand (paperback)",
+      "Bok: Robert Jordan: The Fires of Heaven (paperback) #5",
+    ],
+  },
+  {
+    name: "Helena",
+    wishes: ["videos from when I was little ", "Anti by Rihanna"],
+  },
+  {
+    name: "Kine",
+    wishes: [
+      "Har alt jeg trenger, så ønsker meg samvær og hygge og noen lange klemmer etterhvert ",
+      "At du ikke skal bruke tid eller penger på en ting til meg.",
+      "Noe hjemmelaget, spiselig, drikkbart, en boks med te, sjokolade..",
+    ],
+  },
+  { name: "Lilja", wishes: ["wish 1", "wish 2", "wish 3"] },
+  { name: "User 5", wishes: ["wish 1", "wish 2", "wish 3"] },
 ];
 
 export default MOCK_DATA;
