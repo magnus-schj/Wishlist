a45c73e2c5deaf0367b16352a207ce77c25a3404
Bjorn Solberg <bjorn+git@famsolberg.com>
2021-04-15 01:09:39 +0200

    Initial revision

---
+++ mysql/mysql-fill.sql
@@ -0,0 +1,14 @@
+
+USE `wishlist` ;
+
+INSERT INTO users VALUES (1, 'Bjorn', 'b@c', '2020-01-01', '2021-01-01');
+INSERT INTO users VALUES (2, 'Magnus', 'm@c', '2020-01-01', '2021-01-01');
+
+INSERT INTO password VALUES (1, 'hei', '2020-01-01', '2021-01-01');
+INSERT INTO password VALUES (2, 'hopp', '2020-01-01', '2021-01-01');
+
+INSERT INTO wishlist VALUES (1, 1, 'b en ny', '2020-01-01', '2021-01-01');
+INSERT INTO wishlist VALUES (2, 1, 'b enda en ny', '2020-01-01', '2021-01-01');
+INSERT INTO wishlist VALUES (3, 2, 'm en ny', '2020-01-01', '2021-01-01');
+INSERT INTO wishlist VALUES (4, 2, 'm enda en ny', '2020-01-01', '2021-01-01');
+
