917b2452f370abadda64bc37106ccb803b5eb12d
Magnus <beachmagnus+git@gmail.com>
2021-05-07 23:57:51 +0200

    cant use any url

--- frontend/react/main/src/pages/wish-page/WishPage.component.jsx
+++ frontend/react/main/src/pages/wish-page/WishPage.component.jsx
@@ -10,7 +10,13 @@ const WishPage = ({ match }) => {
   console.log(userInfo);
   return (
     <div className="wish-page">
-      <h1>{userInfo.name}'s wishes</h1>
+      {userInfo ? (
+        <div className="wish-page">
+          <h1>{userInfo.name}'s wishes</h1>
+        </div>
+      ) : (
+        <h1>Denne siden finnes ikke!</h1>
+      )}
     </div>
   );
 };
