892a704f1c7ec2518e39be6ba1a5642a374ae52b
Magnus <beachmagnus+git@gmail.com>
2021-07-08 11:11:56 +0200

    log in logic moved down

--- frontend/react/main/src/App.jsx
+++ frontend/react/main/src/App.jsx
@@ -1,50 +1,17 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setCurrentUser } from "./features/currentUser/currentUser.slice";
-import { auth, signInWithGoogle } from "./firebase/firebase.utils";
-
-import { CssBaseline, Button } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, Container } from "@material-ui/core";
 import SignInAndSignUp from "./components/signInAndSignUp/SignInAndSignUp.component";
 
 function App() {
-  const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.currentUser.userInfo);
-  // const [currentUser, setCurrentUser] = useState(null);
-  let unsubscribeFromAuth = null;
-  useEffect(() => {
-    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      // setCurrentUser(user);
-      dispatch(setCurrentUser(user));
-      // console.log(user.uid);
-      return () => {
-        unsubscribeFromAuth();
-      };
-    });
-  }, []);
   return (
     <>
       <CssBaseline />
-      <div className="App">
+      <Container className="App">
         <h1>App</h1>
-        {currentUser ? (
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => auth.signOut()}
-          >
-            Sign out
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={signInWithGoogle}
-          >
-            Sign in with google
-          </Button>
-        )}
         <SignInAndSignUp />
-      </div>
+      </Container>
     </>
   );
 }
--- frontend/react/main/src/components/signInAndSignUp/SignInAndSignUp.component.jsx
+++ frontend/react/main/src/components/signInAndSignUp/SignInAndSignUp.component.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentUser } from "../../features/currentUser/currentUser.slice";
+
+import { signInWithGoogle, auth } from "../../firebase/firebase.utils";
 
 import { Container, Button } from "@material-ui/core";
 
 const SignInAndSignUp = () => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentUser.userInfo);
+
+  let unsubscribeFromAuth = null;
+  useEffect(() => {
+    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      dispatch(setCurrentUser(user));
+      return () => {
+        unsubscribeFromAuth();
+      };
+    });
+  }, []);
   return (
     <Container className="">
-      <Button></Button>
+      {currentUser ? (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => auth.signOut()}
+        >
+          Sign out
+        </Button>
+      ) : (
+        <Button variant="contained" color="primary" onClick={signInWithGoogle}>
+          Sign in with google
+        </Button>
+      )}
     </Container>
   );
 };
