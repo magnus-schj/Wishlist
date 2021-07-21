5d4439f2648d676344ffc5fcc508305f4310db44
Magnus <beachmagnus+git@gmail.com>
2021-07-22 00:53:46 +0200

    user not found handled

--- frontend/react/main/src/components/wishPage/WishPage.component.jsx
+++ frontend/react/main/src/components/wishPage/WishPage.component.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+
 import { Container, Typography } from "@material-ui/core";
 
 const WishPage = ({ routeProps: { match } }) => {
-  console.log("props:", match);
   const { nameValue } = match.params;
+
+  const userInfo = useSelector((state) =>
+    state.allUsers.users.find((user) => user.nameValue === nameValue)
+  );
+  console.log("userInfo:", userInfo);
   return (
     <Container>
-      <Typography variant="h3" color="initial">
-        {nameValue}'s Wish page
-      </Typography>
+      {userInfo ? (
+        <Typography variant="h3" color="initial">
+          {nameValue}'s Wish page
+        </Typography>
+      ) : (
+        <Typography variant="h3" color="initial">
+          404 Denne siden finnes ikke!
+        </Typography>
+      )}
     </Container>
   );
 };
