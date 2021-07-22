314564ee2a189252a114fd9ef8b937b92d092573
Magnus <beachmagnus+git@gmail.com>
2021-07-22 16:10:30 +0200

    sign in form appearence ok

--- frontend/react/main/src/components/sign-in/SignIn.component.jsx
+++ frontend/react/main/src/components/sign-in/SignIn.component.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useForm } from "../../custom-hooks/useForm";
 
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, TextField } from "@material-ui/core";
 
 const SignIn = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
   return (
     <Container>
       <Typography variant="h3" color="initial">
         Sign in
       </Typography>
+      <form>
+        <TextField
+          id="email"
+          label="Email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </form>
     </Container>
   );
 };
