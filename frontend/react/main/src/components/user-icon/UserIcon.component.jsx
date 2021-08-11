18cca6ccc50a3166003f0ea053228506ef9e6632
Magnus <beachmagnus+git@gmail.com>
2021-08-11 14:10:28 +0200

    user list is now buttons

--- frontend/react/main/src/components/user-icon/UserIcon.component.jsx
+++ frontend/react/main/src/components/user-icon/UserIcon.component.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+import { Button, ButtonBase } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    borderRadius: "90px",
+    height: "5rem",
+    padding: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    margin: 0,
+    padding: 0,
+  },
+});
+
 const UserIcon = ({ name }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>{name.charAt(0)}</h1>
-    </div>
+    <ButtonBase variant="contained" className={classes.root}>
+      <h1 className={classes.header}>{name.charAt(0)}</h1>
+      <h4>{name}</h4>
+    </ButtonBase>
   );
 };
 
--- frontend/react/main/src/components/user-list/UserList.component.jsx
+++ frontend/react/main/src/components/user-list/UserList.component.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import { useSelector } from "react-redux";
 import UserIcon from "../user-icon/UserIcon.component";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "90vw",
+  },
+  header: {
+    textAlign: "center",
+  },
+});
+
 const UserList = () => {
+  const classes = useStyles();
   const allUsers = useSelector((state) => state.allUsers);
   return allUsers.loaded ? (
     <div>
-      <h1>User list</h1>
-      <ul>
+      <h1 className={classes.header}>User list</h1>
+      <div className={classes.root}>
         {allUsers.users.map((user, i) => (
           <UserIcon name={user.nameValue} key={i} />
         ))}
-      </ul>
+      </div>
     </div>
   ) : (
     <h1>Laster...</h1>
