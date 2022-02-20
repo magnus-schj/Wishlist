8467aa6daf36808f620df23d379491c9ec7fd612
Magnus <beachmagnus+git@gmail.com>
2022-02-20 11:49:09 +0100

    most dependencies installed

---
+++ frontend/react/main/src/components/Home.component.ts
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface Props {}
+
+const Home: FC<Props> = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
---
+++ frontend/react/main/src/firebase/firebase.utils.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_9l3UAQx9XkN4GQUrx27uybeOAn3RjF0",
+
+  authDomain: "wishlist-dev-5f414.firebaseapp.com",
+
+  projectId: "wishlist-dev-5f414",
+
+  storageBucket: "wishlist-dev-5f414.appspot.com",
+
+  messagingSenderId: "220708743612",
+
+  appId: "1:220708743612:web:7c1afe7a1ac3ecf09d63b9",
+
+  measurementId: "G-SH12GWBKDW",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
